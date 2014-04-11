ae33e6bf37c75dca95b015de6072daac4d0dc040
ufna <ufna@ufna.ru>
2014-04-11 13:50:30 +0400

    Simple hud class for futher updates

---
+++ Source/SeaCraft/Classes/UI/SeaCraftHUD.h
@@ -0,0 +1,13 @@
+// Copyright 2011-2014 UFNA, LLC. All Rights Reserved.
+
+#include "SeaCraftHUD.generated.h"
+
+/**
+ * Simple hud that processes things like hit notifications, etc.
+ */
+UCLASS()
+class ASeaCraftHUD : public AHUD
+{
+	GENERATED_UCLASS_BODY()
+
+};
---
+++ Source/SeaCraft/Private/UI/SeaCraftHUD.cpp
@@ -0,0 +1,9 @@
+// Copyright 2011-2014 UFNA, LLC. All Rights Reserved.
+
+#include "SeaCraft.h"
+
+ASeaCraftHUD::ASeaCraftHUD(const class FPostConstructInitializeProperties& PCIP) 
+	: Super(PCIP)
+{
+	
+}
