1768f58727aea3c53703f9f5e4a42c0877a9bda0
ufna <ufna@ufna.ru>
2014-04-19 00:30:42 +0400

    Basic struct to keep info about vehicle weapons

--- Source/SeaCraft/Classes/Vehicles/SeaCraftVehicle.h
+++ Source/SeaCraft/Classes/Vehicles/SeaCraftVehicle.h
@@ -5,6 +5,44 @@
 #include "GameFramework/Pawn.h"
 #include "SeaCraftVehicle.generated.h"
 
+namespace EVWeaponState
+{
+	enum Type
+	{
+		Idle,
+		Firing,
+		Reloading,
+		PreFire,
+		PostFire
+	};
+}
+
+/**	The FVWeaponData struct defines each available weapon in the vehicle */
+USTRUCT()
+struct FVWeaponData
+{
+	GENERATED_USTRUCT_BODY()
+
+	/** Vehicle-unique ID to get and/or reference weapon in blueprints */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponInfo)
+	FString WeaponID;
+
+	/** Vehicle-unique group ID to control weapons in group simultaneously */
+	UPROPERTY(EditDefaultsOnly, Category = WeaponInfo)
+	FString GroupID;
+
+	/** Firing sockets names */
+	UPROPERTY(EditDefaultsOnly, Category = Turrets)
+	TArray<FName> TurretSockets;
+
+	/** Defaults */
+	FVWeaponData()
+	{
+		WeaponID = TEXT("MustBeVehicleUnique");
+		GroupID = TEXT("Default");
+	}
+};
+
 /**
  * 
  */
@@ -13,5 +51,8 @@ class ASeaCraftVehicle : public APawn
 {
 	GENERATED_UCLASS_BODY()
 
+	/** Information for each weapon a player may control */
+	UPROPERTY(Category = Vehicle, VisibleDefaultsOnly, BlueprintReadOnly)
+	TArray<FVWeaponData> VehicleWeapons;
 	
 };
