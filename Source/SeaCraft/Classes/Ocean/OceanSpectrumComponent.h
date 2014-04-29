e07047f07b7f363accba4079288c302656052885
ufna <ufna@ufna.ru>
2014-04-29 15:13:30 +0400

    Phillips spectrum and initial H0 calculation

--- Source/SeaCraft/Classes/Ocean/OceanSpectrumComponent.h
+++ Source/SeaCraft/Classes/Ocean/OceanSpectrumComponent.h
@@ -4,6 +4,60 @@
 
 #include "OceanSpectrumComponent.generated.h"
 
+#define PAD16(n) (((n)+15)/16*16)
+
+USTRUCT()
+struct FOceanData
+{
+	GENERATED_USTRUCT_BODY()
+
+	/** The size of displacement map. Must be power of 2. */
+	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	int32 DispMapDimension;
+
+	/** The side length (world space) of square patch. Typical value is 1000 ~ 2000. */
+	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	float PatchLength;
+
+	/** Adjust the time interval for simulation (controls the simulation speed) */
+	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	float TimeScale;
+
+	/** Amplitude for transverse wave. Around 1.0 (not the world space height). */
+	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	float WaveAmplitude;
+
+	/** Wind direction. Normalization not required */
+	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	FVector2D WindDirection;
+
+	/** The bigger the wind speed, the larger scale of wave crest. But the wave scale can be no larger than PatchLength. 
+		Around 100 ~ 1000 */
+	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	float WindSpeed;
+
+	/** This value damps out the waves against the wind direction. Smaller value means higher wind dependency. */
+	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	float WindDependency;
+
+	/** The amplitude for longitudinal wave. Higher value creates pointy crests. Must be positive. */
+	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	float ChoppyScale;
+
+	/** defaults */
+	FOceanData()
+	{
+		DispMapDimension = 512;
+		PatchLength = 2000.0f;
+		TimeScale = 0.8f;
+		WaveAmplitude = 0.35f;
+		WindDirection = FVector2D(0.8f, 0.6f);
+		WindSpeed = 600.0f;
+		WindDependency = 0.07f;
+		ChoppyScale = 1.3f;
+	}
+};
+
 /**
  * Renders normals and heightmap from Phillips spectrum
  */
@@ -19,6 +73,14 @@ class UOceanSpectrumComponent : public UActorComponent
 	/** Render target for height map that can be used by the editor */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = OceanSpectrum)
 	class UTextureRenderTarget2D* HeightTarget;
+
+protected:
+	/** Ocean spectrum data */
+	UPROPERTY(EditDefaultsOnly, Category = Config)
+	FOceanData OceanConfig;
+
+	/** Initialize the vector field */
+	void InitHeightMap(FOceanData& Params, FVector2D* out_h0, float* out_omega);
 
 public:
 	// Begin UActorComponent Interface
@@ -43,8 +105,9 @@ public:
 	/*static*/ void UpdateDeferredSpectrums(/*FSceneInterface* Scene*/);
 
 	/**
-	 * Updates the contents of the given ocean spectrum by processing FFT calculations on spectrum.
-	 * This must be called on the game thread.
-	 */
+	* Updates the contents of the given ocean spectrum by processing FFT calculations on spectrum.
+	* This must be called on the game thread.
+	*/
 	void UpdateOceanSpectrumContents(class UOceanSpectrumComponent* OceanSpectrumComponent);
+
 };
--- Source/SeaCraft/Private/Ocean/OceanSpectrumComponent.cpp
+++ Source/SeaCraft/Private/Ocean/OceanSpectrumComponent.cpp
@@ -4,17 +4,114 @@
 #include "UniformBuffer.h"
 #include "ShaderParameters.h"
 
+#define HALF_SQRT_2	0.7071068f
+#define GRAV_ACCEL	981.0f	// The acceleration of gravity, cm/s^2
+
+#define BLOCK_SIZE_X 16
+#define BLOCK_SIZE_Y 16
+
+/** Generating gaussian random number with mean 0 and standard deviation 1 */
+float Gauss()
+{
+	float u1 = FMath::SRand();
+	float u2 = FMath::SRand();
+
+	if (u1 < 1e-6f)
+	{
+		u1 = 1e-6f;
+	}
+
+	return sqrtf(-2 * logf(u1)) * cosf(2 * PI * u2);
+}
+
+/**
+ * Phillips Spectrum
+ * K: normalized wave vector, W: wind direction, v: wind velocity, a: amplitude constant
+ */
+float Phillips(FVector2D K, FVector2D W, float v, float a, float dir_depend)
+{
+	// Largest possible wave from constant wind of velocity v
+	float l = v * v / GRAV_ACCEL;
+
+	// Damp out waves with very small length w << l
+	float w = l / 1000;
+
+	float Ksqr = K.X * K.X + K.Y * K.Y;
+	float Kcos = K.X * W.X + K.Y * W.Y;
+	float phillips = a * expf(-1 / (l * l * Ksqr)) / (Ksqr * Ksqr * Ksqr) * (Kcos * Kcos);
+
+	// Filter out waves moving opposite to wind
+	if (Kcos < 0)
+	{
+		phillips *= dir_depend;
+	}
+
+	// Damp out waves with very small length w << l
+	return phillips * expf(-Ksqr * w * w);
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Spectrum component
+
 UOceanSpectrumComponent::UOceanSpectrumComponent(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
 	bAutoActivate = true;
 	PrimaryComponentTick.bCanEverTick = true;
 	//PrimaryComponentTick.TickGroup = TG_DuringPhysics;
+
+	// Height map H(0)
+	int32 height_map_size = (OceanConfig.DispMapDimension + 4) * (OceanConfig.DispMapDimension + 1);
+	FVector2D* h0_data = new FVector2D[height_map_size * sizeof(FVector2D)];
+	float* omega_data = new float[height_map_size * sizeof(float)];
+	InitHeightMap(OceanConfig, h0_data, omega_data);
+}
+
+void UOceanSpectrumComponent::InitHeightMap(FOceanData& Params, FVector2D* out_h0, float* out_omega)
+{
+	int32 i, j;
+	FVector2D K, Kn;
+
+	FVector2D wind_dir = Params.WindDirection;
+	wind_dir.Normalize();
+
+	float a = Params.WaveAmplitude * 1e-7f;	// It is too small. We must scale it for editing.
+	float v = Params.WindSpeed;
+	float dir_depend = Params.WindDependency;
+
+	int height_map_dim = Params.DispMapDimension;
+	float patch_length = Params.PatchLength;
+
+	for (i = 0; i <= height_map_dim; i++)
+	{
+		// K is wave-vector, range [-|DX/W, |DX/W], [-|DY/H, |DY/H]
+		K.Y = (-height_map_dim / 2.0f + i) * (2 * PI / patch_length);
+
+		for (j = 0; j <= height_map_dim; j++)
+		{
+			K.X = (-height_map_dim / 2.0f + j) * (2 * PI / patch_length);
+
+			float phil = (K.X == 0 && K.Y == 0) ? 0 : sqrtf(Phillips(K, wind_dir, v, a, dir_depend));
+
+			out_h0[i * (height_map_dim + 4) + j].X = float(phil * Gauss() * HALF_SQRT_2);
+			out_h0[i * (height_map_dim + 4) + j].Y = float(phil * Gauss() * HALF_SQRT_2);
+
+			// The angular frequency is following the dispersion relation:
+			//            out_omega^2 = g*k
+			// The equation of Gerstner wave:
+			//            x = x0 - K/k * A * sin(dot(K, x0) - sqrt(g * k) * t), x is a 2D vector.
+			//            z = A * cos(dot(K, x0) - sqrt(g * k) * t)
+			// Gerstner wave shows that a point on a simple sinusoid wave is doing a uniform circular
+			// motion with the center (x0, y0, z0), radius A, and the circular plane is parallel to
+			// vector K.
+			out_omega[i * (height_map_dim + 4) + j] = sqrtf(GRAV_ACCEL * sqrtf(K.X * K.X + K.Y * K.Y));
+		}
+	}
 }
 
 void UOceanSpectrumComponent::SendRenderTransform_Concurrent()
 {
-	//UpdateContent();
+	UpdateContent();
 
 	Super::SendRenderTransform_Concurrent();
 }
@@ -24,8 +121,7 @@ void UOceanSpectrumComponent::TickComponent(float DeltaTime, enum ELevelTick Tic
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// @TODO Update spectrum, etc.
-	UpdateContent();
-	UpdateDeferredSpectrums();
+	UpdateOceanSpectrumContents(this);
 }
 
 static TArray<UOceanSpectrumComponent*> OceanSpectrumsToUpdate;
@@ -62,8 +158,6 @@ void UOceanSpectrumComponent::PostEditChangeProperty(FPropertyChangedEvent& Prop
 
 void UpdateOceanSpectrumContent_RenderThread(FTextureRenderTargetResource* TextureRenderTarget, FIntRect ViewRect, const FResolveParams& ResolveParams)
 {
-	// UE_LOG(LogOcean, Warning, TEXT("Update spectrum here"));
-
 	RHISetRenderTarget(TextureRenderTarget->GetRenderTargetTexture(), NULL);
 	RHIClear(true, FLinearColor::Yellow, false, 1.0f, false, 0, ViewRect);
 
